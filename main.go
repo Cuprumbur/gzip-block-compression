07b4166a0645a2dd62b6a5661575ecf03b77c6c8
Ochir Darmaev <ochirdarmaev@gmail.com>
2020-03-09 22:53:38 +0300

    refactoring

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"net/http"
 	_ "net/http/pprof"
 	"os"
 	"path"
@@ -18,6 +17,7 @@ import (
 )
 
 func main() {
+	// println(runtime.GOMAXPROCS(10))
 	var pathFile string
 	flag.StringVar(&pathFile, "file", "", "Path of file to compress")
 	flag.Parse()
@@ -33,107 +33,50 @@ func main() {
 
 	r := bufio.NewReader(fileReader)
 
-	in := make(chan []byte, 3)
-	out := make(chan []byte, 3)
+	in := make(chan []byte)
+	out := make(chan []byte)
+	// workers
+	var workerWg sync.WaitGroup
+	workers := 1
+	for i := 0; i < workers; i++ {
+		workerWg.Add(1)
+		go compressWorker(&workerWg, in, out, pathFile)
+	}
+
+	go read(r, in)
+
+	fmt.Println("closed 'out'")
+
+	fileNameWithoutExt := strings.TrimSuffix(pathFile, path.Ext(pathFile))
+
+	outputFile := fileNameWithoutExt + ".gzip"
+	newZipFile, err := os.Create(outputFile)
+	check(err)
+	defer newZipFile.Close()
 	var wg sync.WaitGroup
-
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-
-		for {
-			data := make([]byte, blockSize)
-
-			n, err := r.Read(data)
-
-			if err == io.EOF {
-				fmt.Println("END read")
-				close(in)
-				fmt.Println("closed 'in'")
-				break
-			}
-
-			fmt.Printf("read %d bytes\n", n)
-
-			if n < blockSize {
-				data = data[:n]
-			}
-
-			in <- data
-		}
-
-	}()
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-
-		var wgZip sync.WaitGroup
-		for {
-
-			data, ok := (<-in)
-
-			if !ok {
-				go func() {
-					wgZip.Wait()
-					close(out)
-					fmt.Println("closed 'out'")
-				}()
-				break
-			}
-			wgZip.Add(1)
-			go func() {
-				defer wgZip.Done()
-				var buf bytes.Buffer
-				w := gzip.NewWriter(&buf)
-				w.Name = pathFile
-				n, err := w.Write(data)
-				if err != nil {
-					log.Fatal(err)
-				}
-				w.Close()
-
-				fmt.Printf("compressed %d bytes\n", n)
-
-				select {
-				case out <- buf.Bytes():
-				default:
-				}
-
-			}()
-		}
-	}()
-
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		fileNameWithoutExt := strings.TrimSuffix(pathFile, path.Ext(pathFile))
-
-		outputFile := fileNameWithoutExt + ".gzip"
-		newZipFile, err := os.Create(outputFile)
-		check(err)
-
-		defer newZipFile.Close()
-
-		for {
-			data, ok := (<-out)
-			if !ok {
-				fmt.Println("stop write")
-				break
-			}
+		for data := range out {
 
 			n, err := newZipFile.Write(data)
 			if err != nil {
 				log.Fatal(err)
 			}
+			// _ = n
 			fmt.Printf("wrote %d bytes\n", n)
 		}
 
+		fmt.Println("stop write")
 	}()
-	go func() {
-		http.ListenAndServe("localhost:8080", nil)
-
-	}()
+	workerWg.Wait()
+	close(out)
 	wg.Wait()
+	// go func() {
+	// 	http.ListenAndServe("localhost:8080", nil)
+
+	// }()
+
 	fmt.Printf("done.")
 }
 
@@ -144,3 +87,44 @@ func check(err error) {
 }
 
 var blockSize = 1000000
+
+func read(r *bufio.Reader, in chan<- []byte) {
+	for {
+		data := make([]byte, blockSize)
+
+		n, err := r.Read(data)
+
+		if err == io.EOF {
+			fmt.Println("END read")
+			break
+		}
+		check(err)
+
+		fmt.Printf("read %d bytes\n", n)
+
+		if n < blockSize {
+			data = data[:n]
+		}
+
+		in <- data
+	}
+
+	fmt.Println("closed 'in'")
+	close(in)
+}
+func compressWorker(wg *sync.WaitGroup, in <-chan []byte, out chan<- []byte, pathFile string) {
+	defer wg.Done()
+	for data := range in {
+		var buf bytes.Buffer
+		w := gzip.NewWriter(&buf)
+		defer w.Close()
+		w.Name = pathFile
+		n, err := w.Write(data)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// _ = n
+		fmt.Printf("compressed %d bytes\n", n)
+		out <- buf.Bytes()
+	}
+}
